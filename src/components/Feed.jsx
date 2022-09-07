101cca1d87183a0bd8f1ef7312704da511e9ddb8
Griffin Dooley <dooleygriffin@gmail.com>
2022-09-06 21:58:02 -0400

    fetch call works now

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,16 +6,21 @@ import { Sidebar, Videos } from './';
 
 const Feed = () => {
 
-  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
-  }, []);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }} >
       <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff' }} >
           Copyright 2022 GCD
         </Typography>
@@ -31,14 +36,14 @@ const Feed = () => {
           mb={2}
           sx={{ color: 'white' }}
         >
-          New
+          {selectedCategory}
           <span style={{ color: '#F31503', marginLeft: '10px' }} >
             videos
           </span>
         </Typography>
       </Box>
 
-      <Videos videos={[]} />
+      <Videos videos={videos} />
     </Stack>
   )
 }
